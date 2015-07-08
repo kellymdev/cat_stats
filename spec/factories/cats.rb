c3b7b6435c68c3d6387a237a1008f3c91ddc11a1
Kelly Munro <kellymdev@gmail.com>
2015-07-08 14:03:49 +1200

    Displaying details for a cat

--- spec/factories/cats.rb
+++ spec/factories/cats.rb
@@ -9,6 +9,7 @@ FactoryGirl.define do
     end
 
     factory :deceased_cat do
+      registration_number "LH9876"
       date_of_death "2015-05-21"
     end
   end
---
+++ spec/features/cats/show_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe "Viewing details for a Cat" do
+  let!(:cat) { create(:cat) }
+
+  before do
+    visit "/cats/#{cat.id}"
+  end
+
+  it "displays the pet name for the cat" do
+    within('h2') do
+      expect(page).to have_content(cat.pet_name)
+    end
+  end
+
+  it "displays the date of birth for the cat" do
+    expect(page).to have_content(cat.date_of_birth)
+  end
+
+  context "registered cat" do
+    let!(:registered_cat) { create(:registered_cat) }
+
+    before do
+      visit "/cats/#{registered_cat.id}"
+    end
+
+    it "displays the registered name for the cat" do
+      expect(page).to have_content(registered_cat.registered_name)
+    end
+
+    it "displays the registration number for the cat" do
+      expect(page).to have_content(registered_cat.registration_number)
+    end
+  end
+
+  context "unregistered cat" do
+    before do
+      visit "/cats/#{cat.id}"
+    end
+
+    it "doesn't show the registered name label" do
+      expect(page).to_not have_content("Registered Name:")
+    end
+
+    it "doesn't show the registration number label" do
+      expect(page).to_not have_content("Registration Number:")
+    end
+  end
+
+  context "deceased cat" do
+    let(:deceased_cat) { create(:deceased_cat) }
+
+    before do
+      visit "/cats/#{deceased_cat.id}"
+    end
+
+    it "displays the date of death for the cat" do
+      expect(page).to have_content(deceased_cat.date_of_death)
+    end
+  end
+
+  context "living cat" do
+    before do
+      visit "/cats/#{cat.id}"
+    end
+
+    it "doesn't show the date of death label" do
+      expect(page).to_not have_content("Date of Death:")
+    end
+  end
+end
